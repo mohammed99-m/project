8c867181d98e9fedd1f8f0aeba55e1b8949ec606
mohammed99-m <markoriobrazil@gmail.com>
2024-11-23 17:12:50 +0300

    edit_project

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path , include
 
 urlpatterns = [
-    path('authapp',include('authapp.urls')),
+    path('authapp/',include('authapp.urls')),
     path('admin/', admin.site.urls),
 ]
