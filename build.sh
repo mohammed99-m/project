7e243d133cb8951fde2dd411076c6ed6c4e22ebe
mohammed99-m <markoriobrazil@gmail.com>
2025-05-09 16:11:41 +0300

    add vercel requirments

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#building the project
+cd backend
+echo "building the project..."
+python3 -m ensurepip --default-pip
+echo "pip install start................................................................"
+python3 -m pip install -r ../backend/requirements.txt
+echo "pip install done............"
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput --clear
