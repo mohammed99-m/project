517a9b8cabc997ef56a7e20b88f2589695e0fdfa
mohammed99-m <markoriobrazil@gmail.com>
2025-06-03 23:03:21 +0300

    adding new exercise urls

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 from .views import  list_exercises, recommend_program_ai, search_exercises ,make_program , get_program ,delete_program , update_program , get_coach_programs, update_program_by_days,get_exercises_with_avoid_flag
-from .views import  get_exercises_by_muscle, list_exercises, search_exercises ,make_program , get_program ,delete_program, update_program , get_coach_programs, update_program_by_days,add_exercise_list
+from .views import  get_exercises_by_muscle, list_exercises, search_exercises ,make_program , get_program ,delete_program, update_program , get_coach_programs, update_program_by_days,add_exercise_list,get_exercises_with_avoid_flag_by_muscle
 
 urlpatterns = [
    path('listexercises/',list_exercises,name="List Of Exercises"),
@@ -17,4 +17,5 @@ urlpatterns = [
    path('add-exercises/', add_exercise_list, name='add-exercise-list'),
    path('ai-program/<str:user_id>/', recommend_program_ai, name='ai_program'),
    path('avoidexercises/<str:user_id>/',get_exercises_with_avoid_flag,name='avoided exercises'),
+   path('avoidexercisesbymuscle/<str:user_id>/',get_exercises_with_avoid_flag_by_muscle,name="Avoid Exercises by Muscle")
 ]
