628bf700f9d79090d089320852690af90815ac7b
mohammed99-m <markoriobrazil@gmail.com>
2025-08-02 01:19:46 +0300

    fix

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,6 +3,7 @@ from .views import get_user_notifications, save_notification
 
 urlpatterns = [
     path('save-notification/', save_notification, name='save-notification'),
+    path('save-notification2/', save_notification, name='save-notification'),
     path('user-notifications/<str:receiver_id>/', get_user_notifications, name='user-notifications'),
 
 ]
--- notifications/views.py
+++ notifications/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+
+from accounts.models import Profile
 from .serializers import NotificationSerializer
 from .models import Notification
 from django.contrib.auth import get_user_model
@@ -22,3 +24,34 @@ def get_user_notifications(request, receiver_id):
     notifications = Notification.objects.filter(receiver_id=receiver_id).order_by('-created_at')
     serializer = NotificationSerializer(notifications, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import NotificationSerializer
+from .models import Notification
+
+@api_view(['POST'])
+def save_notification2(request):
+    sender_id = request.data.get('sender')
+    receiver_id = request.data.get('receiver')
+
+    try:
+        sender = Profile.objects.get(User__id=sender_id)
+        receiver = User.objects.get(id=receiver_id)
+    except User.DoesNotExist:
+        return Response({"error": "Sender or receiver not found"}, status=status.HTTP_404_NOT_FOUND)
+
+    # باقي البيانات (مثلاً: content و room_name)
+    content = request.data.get('content')
+    room_name = request.data.get('room_name')
+
+    notification = Notification.objects.create(
+        sender=sender,
+        receiver=receiver,
+        content=content,
+        room_name=room_name
+    )
+
+    return Response({"message": "Notification saved successfully"}, status=status.HTTP_201_CREATED)
